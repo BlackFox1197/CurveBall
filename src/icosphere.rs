39b2fcda056084409e2c0674ead7016e5a608f19
Boris-Chengbiao Zhou <bobo1239@web.de>
2018-07-19 01:14:58 +0200

    Optimize icosphere generation

--- src/icosphere.rs
+++ src/icosphere.rs
@@ -1,5 +1,6 @@
 use cgmath::InnerSpace;
 use cgmath::Vector3;
+use std::collections::HashMap;
 
 use crate::vertex::Vertex;
 
@@ -7,10 +8,39 @@ pub fn icosphere(subdivison_level: u8) -> (Vec<Vertex>, Vec<u32>) {
     let (mut vertices, mut indices) = create_icosahedron();
 
     let radius = vertices[0].magnitude();
-    let mut num_triangles = 20;
+    let mut num_faces = 20;
+    debug_assert_eq!(num_faces * 3, indices.len());
+
+    let final_faces = num_faces * (4usize.pow(subdivison_level.into()));
+    // Euler's formula: Vertices = 2 + Edges - Faces
+    let final_vertices = 2 + (final_faces * 3 / 2) - final_faces;
+    let final_indices = final_faces * 3;
+    vertices.reserve_exact(final_vertices - vertices.len());
+    indices.reserve_exact(final_indices - indices.len());
+
+    let mut vertex_cache: HashMap<(u32, u32), u32> = HashMap::new();
+    let mut get_middle_point = |mut p0_idx: u32, mut p1_idx: u32| {
+        if p1_idx < p0_idx {
+            std::mem::swap(&mut p0_idx, &mut p1_idx);
+        }
+
+        match vertex_cache.get(&(p0_idx, p1_idx)) {
+            Some(mid_idx) => *mid_idx,
+            None => {
+                let p0 = &vertices[p0_idx as usize];
+                let p1 = &vertices[p1_idx as usize];
+                let mid = (p0 + 0.5 * (p1 - p0)).normalize_to(radius);
+
+                let index = vertices.len();
+                vertices.push(mid);
+                vertex_cache.insert((p0_idx, p1_idx), index as u32);
+                index as u32
+            }
+        }
+    };
 
     for _ in 0..subdivison_level {
-        for offset in (0..num_triangles).map(|x| x * 3) {
+        for offset in (0..num_faces).map(|x| x * 3) {
             //           p0
             //           /\
             //          /  \
@@ -26,21 +56,9 @@ pub fn icosphere(subdivison_level: u8) -> (Vec<Vertex>, Vec<u32>) {
             let p1_idx = indices[offset + 1];
             let p2_idx = indices[offset + 2];
 
-            let p0 = &vertices[p0_idx as usize];
-            let p1 = &vertices[p1_idx as usize];
-            let p2 = &vertices[p2_idx as usize];
-
-            let p3_idx = vertices.len() as u32;
-            let p4_idx = (vertices.len() + 1) as u32;
-            let p5_idx = (vertices.len() + 2) as u32;
-
-            let p3 = (p0 + 0.5 * (p1 - p0)).normalize_to(radius);
-            let p4 = (p1 + 0.5 * (p2 - p1)).normalize_to(radius);
-            let p5 = (p2 + 0.5 * (p0 - p2)).normalize_to(radius);
-
-            vertices.push(p3);
-            vertices.push(p4);
-            vertices.push(p5);
+            let p3_idx = get_middle_point(p0_idx, p1_idx);
+            let p4_idx = get_middle_point(p1_idx, p2_idx);
+            let p5_idx = get_middle_point(p2_idx, p0_idx);
 
             indices[offset + 1] = p3_idx;
             indices[offset + 2] = p5_idx;
@@ -57,12 +75,16 @@ pub fn icosphere(subdivison_level: u8) -> (Vec<Vertex>, Vec<u32>) {
             indices.push(p5_idx);
             indices.push(p3_idx);
         }
-        num_triangles *= 4;
+        num_faces *= 4;
     }
+
+    debug_assert_eq!(final_vertices, vertices.len());
+    debug_assert_eq!(final_indices, indices.len());
 
     (vertices.into_iter().map(|v| v.into()).collect(), indices)
 }
 
+// TODO: replace with static or const
 fn create_icosahedron() -> (Vec<Vector3<f32>>, Vec<u32>) {
     let tau = (1. + 5f32.sqrt()) / 2.;
 
